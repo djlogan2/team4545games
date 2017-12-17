67479d5901515c96271d2ee484c21947b6ca8cc9
David Logan <djlogan2.dl@gmail.com>
2017-12-17 08:22:14 -0700

    Initial commit

---
+++ games.js
@@ -0,0 +1,101 @@
+'use strict';
+const browser = new (require('zombie'))();
+const google = require('googleapis');
+const googleAuth = require('google-auth-library');
+const fs = require('fs');
+const GMail = require('./gmail');
+const Joda = require('js-joda').use(require('js-joda-timezone'));
+const _ = require('underscore');
+const now = new Date();
+const znow = Joda.ZonedDateTime.now(Joda.ZoneId.of('America/New_York'));
+const FORMATTER = require('js-joda').DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXXXX");
+let settings = {username: '', teams: []};
+
+fs.readFile('settings.json', function(err, data){
+    if(!err)
+        settings = JSON.parse(data);
+});
+
+function getGames() {
+    var records = [];
+    return new Promise(function(resolve, reject){
+        browser.visit('http://team4545league.org/tournament/games.html')
+            .then(function(){
+                const nodes = browser.querySelectorAll('tr');
+                for(let x = 2 ; x < nodes.length ; x++)
+                {
+                    // Sat, Dec 16, 07:00
+                    // 'Sat, Dec 16, 07:00'.split(/[ ,:]/)
+                    // [ 'Sat', '', 'Dec', '16', '', '07', '00' ]
+                    const dt = nodes[x].cells[2].textContent.split(/[ ,:]/);
+                    const month = 'JanFebMarAprMayJunJulAugSepOctNovDec'.indexOf(dt[2]) / 3 + 1;
+                    const day = dt[3] - 1;
+                    const hour = dt[5];
+                    const min = dt[6];
+                    const year = now.getFullYear() + (now.getMonth() > month ? 1 : 0);
+                    const when = Joda.ZonedDateTime.of(year, month, day, hour, min, 0, 0, Joda.ZoneId.of('America/New_York'));
+                    const record = {
+                        division: nodes[x].cells[0].textContent,
+                        round: nodes[x].cells[1].textContent,
+                        when: when,
+                        white_team: nodes[x].cells[3].textContent,
+                        black_team: nodes[x].cells[7].textContent,
+                        board: nodes[x].cells[8].textContent,
+                        white: nodes[x].cells[4].textContent,
+                        black: nodes[x].cells[6].textContent
+                    };
+                    if(record.when.isAfter(znow) &&
+                        record.white !== settings.username && record.black !== settings.username &&
+                        (!settings || !settings.teams || !settings.teams.length ||
+                            _.indexOf(settings.teams, record.white_team) != -1 || _.indexOf(settings.teams, record.black_team) != -1))
+                        records.push(record);
+                }
+                resolve(records);
+            })
+            .catch(e => reject(e));
+    });
+}
+
+const gmail = new GMail();
+var events = null;
+var leftover_games = [];
+
+function dteq(date1, date2)
+{
+    if(typeof date1 === 'string') date1 = Joda.ZonedDateTime.parse(date1);
+    if(typeof date2 === 'string') date2 = Joda.ZonedDateTime.parse(date2);
+    return date1.format(FORMATTER) === date2.format(FORMATTER);
+}
+
+function otherteam(record) {
+    if(!settings || !settings.teams || !settings.teams.length || _.findIndex(settings.teams, record.white_team) != -1) return record.black_team;
+    else return record.white_team;
+}
+gmail.authenticate()
+    .then(() => gmail.setCalendar('team4545'))
+    .then(() => gmail.list())
+    .then(function(items) { events = items ; return getGames(); })
+    .then(function(games) {
+        games.forEach(function(game){
+            const summary = gmail.getSummary(otherteam(game), game.white, game.black, game.when);
+            console.log('Looking for event ' + summary + ' ' + game.when);
+            const event = _.find(events, function(e){console.log(e.summary + ' === ' + summary + ' , ' + e.start.dateTime + ' ' + game.when + ' ' + dteq(e.start.dateTime, game.when)); return e.summary === summary && dteq(e.start.dateTime, game.when);});
+            if(event) {
+                console.log('found event ' + summary);
+                const i = _.findIndex(events, function(e) {return e.summary === event.summary && dteq(e.start.dateTime, game.when);});
+                console.log('removing event[' + i + '] from the events array');
+                events.splice(i, 1);
+            } else {
+                console.log('did not find event ' + summary);
+                leftover_games.push(game);
+            }
+        });
+        let promises = [];
+        leftover_games.forEach(function(game){
+            promises.push(gmail.insert(otherteam(game), game.white, game.black, game.when));
+        });
+        events.forEach(function(event){
+            promises.push(gmail.delete(event));
+        });
+        return Promise.all(promises);
+    }).then(function(){console.log('done');process.exit(0);});
---
+++ gmail.js
@@ -0,0 +1,164 @@
+const fs = require('fs');
+const ZonedDateTime = require('js-joda').ZonedDateTime;
+const FORMATTER = require('js-joda').DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXXXX");
+
+function Calendar() {
+    this.google = require('googleapis');
+    this.calendarId = 'primary';
+}
+
+Calendar.prototype.authenticate = function () {
+    const self = this;
+
+    function getClientSecret() {
+        return new Promise(function (resolve, reject) {
+            fs.readFile('client_secret.json', function (err, data) {
+                if (err) {
+                    throw new Error(err);
+                    process.exit(0);
+                }
+                resolve(JSON.parse(Buffer.from(data).toString()));
+            });
+        });
+    };
+
+    function askUserToGetToken() {
+        const authUrl = self.oauth2Client.generateAuthUrl({
+            access_type: 'offline',
+            scope: ['https://www.googleapis.com/auth/calendar']
+        });
+        console.log('Please go to this URL, and then run me again with the token as a parmeter:');
+        console.log(authUrl);
+        process.exit(0);
+    }
+
+    function resolveAccessToken() {
+        return new Promise(function (resolve, reject) {
+            // Step 2: Try to load a previously saved access token
+            fs.readFile('access_token.json', function (err, data) {
+                if (err) {
+                    if (process.argv.length === 3) {
+                        self.oauth2Client.getToken(process.argv[2], function (err, token) {
+                            if (err) {
+                                throw new Error(err);
+                                process.exit(0);
+                            } else {
+                                self.oauth2Client.credentials = token;
+                                fs.writeFile('access_token.json', JSON.stringify(token), function (err) {
+                                    if (err) {
+                                        throw new Error(err);
+                                        process.exit(0);
+                                    }
+                                    else resolve();
+                                });
+                            }
+                        });
+                    } else {
+                        askUserToGetToken();
+                    }
+                } else {
+                    self.oauth2Client.credentials = JSON.parse(data);
+                    resolve();
+                }
+            });
+        });
+    }
+
+    return getClientSecret()
+        .then(function (secret) {
+            self.oauth2Client = new self.google.auth.OAuth2(secret.installed.client_id, secret.installed.client_secret, secret.installed.redirect_uris[0]);
+            self.google.options({auth: self.oauth2Client});
+            return resolveAccessToken();
+        });
+};
+
+Calendar.prototype.setCalendar = function(find_str)
+{
+    const self = this;
+    return new Promise(function(resolve, reject){
+        if(find_str === 'primary') { resolve('primary'); return; }
+        const calendar = self.google.calendar('v3');
+        calendar.calendarList.list({}, function(err, response){
+            if(err) {
+                console.log(new Error('The API returned an error: ' + err));
+                reject(new Error(err));
+                return;
+            }
+            for(let x = 0 ; x < response.items.length ; x++) {
+                if(find_str === response.items[x].summary) {
+                    self.calendarId = response.items[x].id;
+                    resolve(response.items[x].id);
+                    return;
+                }
+            }
+            reject(new Error('No calendar found with a summary of ' + find_str));
+        });
+    });
+};
+
+Calendar.prototype.delete = function(event) {
+    const self = this;
+    return new Promise(function(resolve,reject){
+        const calendar = self.google.calendar('v3');
+        calendar.events.list({
+            calendarId: self.calendarId,
+            eventId: event.id
+        }, function(err){
+            if(err) reject(err); else resolve();
+        });
+    });
+};
+
+Calendar.prototype.list = function () {
+    const self = this;
+    return new Promise(function(resolve, reject){
+        const calendar = self.google.calendar('v3');
+        calendar.events.list({
+            calendarId: self.calendarId,
+            timeMin: (new Date()).toISOString(),
+            singleEvents: true,
+            orderBy: 'startTime'
+        }, function (err, response) {
+            if (err) {
+                console.log('The API returned an error: ' + err);
+                reject(new Error(err));
+                return;
+            }
+            resolve(response.items);
+        });
+    });
+};
+
+Calendar.prototype.getSummary = function(team, white, black, start) {
+    return 'T4545: ' + white + '-' + black + '(' + team + ')';
+};
+
+Calendar.prototype.insert = function(team, white, black, start) {
+    const self = this;
+    const event = {
+        calendarId: self.calendarId,
+        summary: self.getSummary(team, white, black, start),
+        // This is the 'notes' field  'description':  '[description] T4545: ' + white + '-' + black + '(' + team + ')',
+        start: {
+            dateTime: start.format(FORMATTER),
+            //'timeZone': 'America/New_York',
+        },
+        end: {
+            dateTime: (start.plusHours(3)).format(FORMATTER),
+            //'timeZone': 'America/New_York',
+        }
+    };
+    return new Promise(function(resolve, reject){
+        const calendar = self.google.calendar('v3');
+        calendar.events.insert({calendarId: self.calendarId, resource: event}, function(err, event){
+            if(err) {
+                console.log('The API returned an error: ' + err);
+                reject(new Error(err));
+                return;
+            }
+            resolve(event);
+        });
+    });
+};
+
+module.exports = Calendar;
